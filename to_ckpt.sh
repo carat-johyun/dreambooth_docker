4c2f718908ddb0b436f4fe3ba139b1903f705828
carat-johyun <johyun@carat.im>
2023-03-06 22:29:11 +0900

    800 스텝 버그 수정

--- to_ckpt.sh
+++ to_ckpt.sh
@@ -1,1 +1,3 @@
-python /tmp/convert_diffusers_to_original_stable_diffusion.py --model_path /home/ubuntu/output/8  --checkpoint_path /home/ubuntu/output/8/sks.ckpt --half
+python /tmp/convert_diffusers_to_original_stable_diffusion.py \
+  --model_path /home/ubuntu/output/800 \
+  --checkpoint_path /home/ubuntu/output/800/sks.ckpt --half
