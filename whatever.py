c493ef1d54e1416c86fa41930b2b71c6f4f699c3
carat-johyun <johyun@carat.im>
2023-03-07 10:36:15 +0900

    secret key 숨기기

--- whatever.py
+++ whatever.py
@@ -6,16 +6,18 @@ import datetime
 
 def push_ckpt():
     s3 = boto3.client("s3",
-                      aws_access_key_id="AKIASELGVJXOKTZDSMP2",
-                      aws_secret_access_key="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",
+                      aws_access_key_id="${{ secrets.AWS_ACCESS_KEY_ID }}",
+                      aws_secret_access_key="${{ secrets.AWS_SECRET_ACCESS_KEY }}",
                       )
 
     date = datetime.datetime.today().strftime('%Y%m%d%H%M%S')
 
+    ckpt_path = "dreambooth/asim_" + date + ".ckpt"
+
     s3.upload_file(
         Filename="/home/ubuntu/output/80/asim.ckpt",
         Bucket="carat-assets",
-        Key="dreambooth/asim_" + date + ".ckpt",
+        Key=ckpt_path,
     )
 
     s3.upload_file(
@@ -39,6 +41,8 @@ def push_ckpt():
         Key="dreambooth/samples/" + date + "_3.png",
     )
 
+    return {"output": {"ckpt": ckpt_path, "samples": "dreambooth/samples/" + date + "_0.png"}}
+
 
 def is_even(job):
     job_input = job["input"]
@@ -55,12 +59,7 @@ def is_even(job):
     subprocess.run(["sh", "/home/ubuntu/train.sh"])
     subprocess.run(["sh", "/home/ubuntu/to_ckpt.sh"])
 
-    push_ckpt()
-
-    if the_number % 2 == 0:
-        return True
-
-    return False
+    return push_ckpt()
 
 
 runpod.serverless.start({"handler": is_even})
