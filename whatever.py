a4b453c27cb5d481d186c432fdb41fe6086ef365
carat-johyun <johyun@carat.im>
2023-03-07 14:53:02 +0900

    ckpt 업로드를 맨 마지막 순서로 변경

--- whatever.py
+++ whatever.py
@@ -15,12 +15,6 @@ def push_ckpt(access_key, secret_access_key):
     ckpt_path = "dreambooth/asim_" + date + ".ckpt"
 
     s3.upload_file(
-        Filename="/home/ubuntu/output/800/asim.ckpt",
-        Bucket="carat-assets",
-        Key=ckpt_path,
-    )
-
-    s3.upload_file(
         Filename="/home/ubuntu/output/800/samples/0.png",
         Bucket="carat-assets",
         Key="dreambooth/samples/" + date + "_0.png",
@@ -39,6 +33,11 @@ def push_ckpt(access_key, secret_access_key):
         Filename="/home/ubuntu/output/800/samples/3.png",
         Bucket="carat-assets",
         Key="dreambooth/samples/" + date + "_3.png",
+    )
+    s3.upload_file(
+        Filename="/home/ubuntu/output/800/asim.ckpt",
+        Bucket="carat-assets",
+        Key=ckpt_path,
     )
 
     return {"output": {"ckpt": ckpt_path, "samples": "dreambooth/samples/" + date + "_0.png"}}
