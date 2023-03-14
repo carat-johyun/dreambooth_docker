dd7d57452826a0a25b585f82da302b287a3c39c2
carat-johyun <johyun@carat.im>
2023-03-14 16:54:43 +0900

    딱 185개 클래스 이미지만 썼을때

--- train.sh
+++ train.sh
@@ -14,7 +14,7 @@ accelerate launch /home/ubuntu/train_dreambooth.py \
   --learning_rate=1e-6 \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
-  --num_class_images=400 \
+  --num_class_images=185 \
   --sample_batch_size=4 \
   --max_train_steps=800 \
   --save_interval=10000 \
